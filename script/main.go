0291499a6413d5c9b176d999c007254079658716
Marvin Lee Fimmel <kontakt@ekranos.me>
2025-08-17 14:55:44 +0200

    .

--- script/main.go
+++ script/main.go
@@ -14,11 +14,25 @@ func Add(a, b int) int {
 func AddWithTime(a, b int) int64 {
 	result := a + b
 	timestamp := ffi.GetCurrentTime()
+
 	return int64(result) + timestamp
 }
 
+var allocatedMemory = make(map[unsafe.Pointer][]byte)
+
 //export allocate
 func Allocate(size int) unsafe.Pointer {
+	data := make([]byte, size)
+	ptr := unsafe.Pointer(&data[0])
+
+	allocatedMemory[ptr] = data
+
+	return ptr
+}
+
+//export deallocate
+func Deallocate(ptr unsafe.Pointer) {
+	delete(allocatedMemory, ptr)
 }
 
 func main() {}
