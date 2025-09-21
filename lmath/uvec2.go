f9872cb28f6da29724cb8d8f7e12b3986ad069f0
Marvin Lee Fimmel <kontakt@ekranos.me>
2025-09-21 03:11:45 +0200

    .

--- lmath/uvec2.go
+++ lmath/uvec2.go
@@ -1,8 +1,6 @@
 package lmath
 
 import (
-	"fmt"
-
 	"github.com/oriolus-software/script-go/internal/msgpack"
 )
 
@@ -11,42 +9,13 @@ type UVec2 struct {
 	Y uint
 }
 
-func (v *UVec2) UnmarshalMsgpack(r *msgpack.Reader) error {
-	length, err := r.ReadArrayHeader()
-	if err != nil {
-		return err
-	}
-
-	if length != 2 {
-		return fmt.Errorf("expected 2 elements, got %d", length)
-	}
-
-	x, err := r.ReadValue()
-	if err != nil {
-		return err
-	}
-	v.X = x.(uint)
-	y, err := r.ReadValue()
-	if err != nil {
-		return err
-	}
-	v.Y = y.(uint)
-
-	return nil
-}
-
 func (v UVec2) MarshalMsgpack(w *msgpack.Writer) error {
 	if err := w.WriteArrayHeader(2); err != nil {
 		return err
 	}
 
-	if err := w.WriteUint(uint64(v.X)); err != nil {
-		return err
-	}
-
-	if err := w.WriteUint(uint64(v.Y)); err != nil {
-		return err
-	}
+	w.Encode(v.X)
+	w.Encode(v.Y)
 
 	return nil
 }
