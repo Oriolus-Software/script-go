824dab1b30bfe6c2bfc7ac8ba7f5d6ed4ab1e221
Marvin Lee Fimmel <kontakt@ekranos.me>
2025-09-24 18:52:42 +0200

    .

--- lmath/vec2.go
+++ lmath/vec2.go
@@ -1,8 +1,6 @@
 package lmath
 
 import (
-	"fmt"
-
 	"github.com/oriolus-software/script-go/internal/msgpack"
 )
 
@@ -12,25 +10,25 @@ type Vec2 struct {
 }
 
 func (v *Vec2) UnmarshalMsgpack(r *msgpack.Reader) error {
-	l, err := r.ReadArrayHeader()
-	if err != nil {
-		return err
-	}
+	// l, err := r.ReadArrayHeader()
+	// if err != nil {
+	// 	return err
+	// }
 
-	for i := 0; i < l; i++ {
-		switch i {
-		case 0:
-			v.X, err = r.ReadFloat32()
-		case 1:
-			v.Y, err = r.ReadFloat32()
-		default:
-			return fmt.Errorf("expected 2 elements, got %d", l)
-		}
+	// for i := 0; i < l; i++ {
+	// 	switch i {
+	// 	case 0:
+	// 		v.X, err = r.ReadFloat32()
+	// 	case 1:
+	// 		v.Y, err = r.ReadFloat32()
+	// 	default:
+	// 		return fmt.Errorf("expected 2 elements, got %d", l)
+	// 	}
 
-		if err != nil {
-			return err
-		}
-	}
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// }
 
 	return nil
 }
