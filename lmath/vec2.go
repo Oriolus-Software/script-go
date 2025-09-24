d7b07fce50df6a4409181c687620b61d22f68ad0
Marvin Lee Fimmel <kontakt@ekranos.me>
2025-09-24 19:23:02 +0200

    .

--- lmath/vec2.go
+++ lmath/vec2.go
@@ -17,19 +17,18 @@ func (v *Vec2) UnmarshalMsgpack(r *msgpack.Reader) error {
 		return err
 	}
 
-	for i := 0; i < l; i++ {
-		switch i {
-		case 0:
-			v.X, err = r.ReadFloat32()
-		case 1:
-			v.Y, err = r.ReadFloat32()
-		default:
-			return fmt.Errorf("expected 2 elements, got %d", l)
-		}
+	if l != 2 {
+		return fmt.Errorf("expected 2 elements, got %d", l)
+	}
 
-		if err != nil {
-			return err
-		}
+	v.X, err = r.ReadFloat32()
+	if err != nil {
+		return err
+	}
+
+	v.Y, err = r.ReadFloat32()
+	if err != nil {
+		return err
 	}
 
 	return nil
